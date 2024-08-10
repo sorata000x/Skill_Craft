fff58d2408b16aae8a52ffb81c84bf426bba938c
Sora <70079892+sorata000x@users.noreply.github.com>
2024-08-10 15:29:17 -0700

    Initial Commit

---
+++ Skill Craft/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Skill Craft
+//
+//  Created by Sora Izayoi on 8/10/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Skill Craft/Skill_CraftApp.swift
@@ -0,0 +1,17 @@
+//
+//  Skill_CraftApp.swift
+//  Skill Craft
+//
+//  Created by Sora Izayoi on 8/10/24.
+//
+
+import SwiftUI
+
+@main
+struct Skill_CraftApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
